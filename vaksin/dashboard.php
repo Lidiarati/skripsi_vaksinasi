3b65bd12330bae15e37778493fc2d201ce0b68f1
Lidiarati <ratinduet99@gmail.com>
2022-06-28 15:43:08 +0700

    satu

--- vaksin/dashboard.php
+++ vaksin/dashboard.php
@@ -25,7 +25,7 @@
   <link rel="icon" type="image/png" href="./assets/img/vaccine.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Vaksin | Dashboard User 
+    Vaksin | Dashboard User Vaksinasi
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   
